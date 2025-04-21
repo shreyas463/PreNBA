c8d73dc5827bc9430f23f089cd7df0067cc5ed85
shreyas463 <shreyaschaudhary3@gmail.com>
2025-04-20 22:45:40 -0700

    new changes to key

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
-export const API_KEY = '2bbe357a19a52532994d737d16669400';
+export const API_KEY = '796f81d5c7ffa9fafdcaf69361eb9b8e';
 export const FOOTBALL_API_URL = 'https://v3.football.api-sports.io';
 export const NBA_API_URL = 'https://v3.basketball.api-sports.io';
